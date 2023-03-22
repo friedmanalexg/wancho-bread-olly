65f8daee38d1c43657a0f2c0be47d72e9f3ed854
Alex <friedmanalexg@gmail.com>
2023-03-22 13:51:44 -0400

    demo candidate for testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://wancho-bread-olly-default-rtdb.firebaseio.com/"
@@ -25,14 +25,20 @@ addButtonEl.addEventListener("click", function() {
 onValue(shoppingListInDB, function(snapshot){
    
     let shopListArray = Object.entries(snapshot.val())
+    if (snapshot.exists()) {
+        
     
-    clearShoppingListEl()
+        clearShoppingListEl()
 
-    for (let i = 0; i < shopListArray.length; i++) {
-        let currentItem = shopListArray[i]
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
-        appendItemToShoppingListEl(currentItemValue)
+        for (let i = 0; i < shopListArray.length; i++) {
+            let currentItem = shopListArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            appendItemToShoppingListEl(currentItem)
+        }
+    }
+    else{
+        shoppingListEl.innerHTML = "No items added yet!"
     }
 })
 
@@ -44,6 +50,17 @@ function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(itemValue) {
-    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+    newEl.addEventListener("click", function(){
+        let itemDBAddy = ref(database, `shoppingList/${itemID}`)
+        remove(itemDBAddy)
+    })
+
+    shoppingListEl.append(newEl)
 }
